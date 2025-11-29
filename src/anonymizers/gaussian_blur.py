4ad1743757ca660552ca5187b1d20f629e1e332d
GiovanniCorda <giovanni.corda3199@gmail.com>
2025-11-29 20:06:08 +0100

    Made adaptive the anonymizer (Gaussian Blur)

--- src/anonymizers/gaussian_blur.py
+++ src/anonymizers/gaussian_blur.py
@@ -1,13 +1,14 @@
 import cv2
 from matplotlib.pyplot import box
 
-def apply_gaussian_blur(img, box, kernel_size=(15, 15)):
+def apply_gaussian_blur(img, box, strength=0.9):
     """
     Applica un filtro gaussiano a una regione specificata dell'immagine (ROI).
-
+    Il kernel sarà adattivo in base alla dimensione della ROI e alla forza specificata.
+    
     img: immagine di input (array numpy).
     box: tuple/list con le coordinate della ROI (x1, y1, x2, y2).
-    kernel_size: dimensione del kernel per il filtro gaussiano. (Non deve essere dispari perché cv2 lo richiede.)
+    strength: intensità del filtro gaussiano (valore tra 0 e 1).
 
     Ritorna l'immagine con la ROI sfocata applicata sull'immagine originale.
     """
@@ -25,8 +26,17 @@ def apply_gaussian_blur(img, box, kernel_size=(15, 15)):
     # Estrai la regione di interesse (ROI) dall'immagine
     roi = img[y1:y2, x1:x2]
     
+    rh, rw = roi.shape[:2]
+    # kernel proporzionale alla dimensione più grande della faccia
+    k = int(max(rh, rw) * strength)
+
+    # forza il kernel ad essere dispari e con un minimo sufficiente per anonimizzare
+    if k % 2 == 0:
+        k += 1
+    k = max(k, 31)
+
     # Applica il filtro gaussiano alla ROI
-    blurred_roi = cv2.GaussianBlur(roi, kernel_size, 0)
+    blurred_roi = cv2.GaussianBlur(roi, (k, k), 0)
     
     # Sostituisci la ROI originale con quella sfocata nell'immagine
     img[y1:y2, x1:x2] = blurred_roi
