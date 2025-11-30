a53bb0c252cfe79101d603255e0f3b85497cfe50
GiovanniCorda <giovanni.corda3199@gmail.com>
2025-11-30 12:28:05 +0100

    Add utility function video writer

--- src/utils/io_utils.py
+++ src/utils/io_utils.py
@@ -11,7 +11,7 @@ def open_video(input_path):
     - fps: fotogrammi al secondo
     - dimensioni: (larghezza, altezza)
     - total_frames: numero totale di fotogrammi
-    
+
     """
     if not os.path.exists(input_path):
         raise FileNotFoundError(f"Il file video non esiste: {input_path}")
@@ -27,3 +27,22 @@ def open_video(input_path):
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     
     return cap, fps, (width, height), total_frames
+
+def create_video_writer(output_path, fps, frame_size):
+    """
+    Crea un oggetto VideoWriter di OpenCV per salvare un file video.
+
+    output_path: percorso del file video di output.
+    fps: fotogrammi al secondo.
+    frame_size: dimensioni del fotogramma (larghezza, altezza).
+
+    Ritorna l'oggetto VideoWriter di OpenCV.
+    """
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec per file mp4 (tra i più comuni)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True) # crea la cartella se non esiste
+    out = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
+    
+    if not out.isOpened():
+        raise RuntimeError(f"Impossibile creare il file video: {output_path}")
+    
+    return out
